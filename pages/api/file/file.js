02b93c45dab264373b32f75addeda5ee6c640397
Bhuneshar <imankitkalirawana@gmail.com>
2023-12-19 12:32:16 +0530

    installed aws

--- pages/api/file/file.js
+++ pages/api/file/file.js
@@ -1,19 +1,18 @@
 import path from "path";
 import fs from "fs";
 import multer from "multer";
+import aws from "aws-sdk";
 import verifyToken from "@/middleware/verifyToken";
 
-const storage = multer.diskStorage({
-  destination: path.join(process.cwd(), "public/uploads"),
-  filename: (req, file, callback) => {
-    // Keep the original file name
-    callback(null, file.originalname);
-  },
+const s3 = new aws.S3({
+  accessKeyId: "AKIARVSJ7VXJAJGKXNOE",
+  secretAccessKey: "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",
+  region: "ap-south-1",
 });
 
-const upload = multer({
-  storage: storage,
-}).single("file");
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage }).single("file");
 
 // create upload directory if it doesn't exist
 const dir = path.join(process.cwd(), "public/uploads");
@@ -26,21 +25,30 @@ export default async function handler(req, res) {
     if (req.method === "POST") {
       // upload file to server
       try {
-        await new Promise((resolve, reject) => {
-          upload(req, res, function (err) {
-            if (err instanceof multer.MulterError) {
-              // A Multer error occurred when uploading.
-              reject(err);
-            } else if (err) {
-              // An unknown error occurred.
-              reject(err);
+        // upload to aws s3
+        upload(req, res, (err) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({ error: "Internal Server Error" });
+          }
+          const { originalname, buffer } = req.file;
+          const params = {
+            Bucket: "devocode-resources",
+            Key: originalname,
+            Body: buffer,
+          };
+          s3.upload(params, (err, data) => {
+            if (err) {
+              console.log(err);
+              return res.status(500).json({ error: "Internal Server Error" });
             }
-            // File upload was successful.
-            resolve();
+            const { Location, Key } = data;
+            res.status(200).json({
+              message: "File uploaded successfully.",
+              file: { url: Location, name: Key },
+            });
           });
         });
-        //   do something after the file upload if needed
-        res.status(200).json({ message: "File uploaded successfully." });
       } catch (err) {
         console.log(err);
         res.status(500).json({ error: "Internal Server Error" });
@@ -48,19 +56,27 @@ export default async function handler(req, res) {
     } else if (req.method === "DELETE") {
       // try and catch block
       try {
+        // code to delete file from aws s3
         const { filename } = req.query;
         if (!filename) {
           return res.status(400).json({ error: "Filename is required." });
         }
-        const filePath = path.join(process.cwd(), "public/uploads", filename);
-        if (fs.existsSync(filePath)) {
-          fs.unlinkSync(filePath);
+
+        const deleteParams = {
+          Bucket: "devocode-resources",
+          Key: filename,
+        };
+
+        s3.deleteObject(deleteParams, (err) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({ error: "Internal Server Error" });
+          }
+
           res
             .status(200)
             .json({ message: `File ${filename} deleted successfully.` });
-        } else {
-          res.status(404).json({ error: `File ${filename} not found.` });
-        }
+        });
       } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
       }
